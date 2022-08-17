188c93d0408799900eb534ba840ea82704208bae
a7853z <549613345@qq.com>
2022-08-17 17:55:36 +0800

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -17,6 +17,8 @@
 #include "pir.h"
 #include "pir_client.h"
 #include "SHA256.h"
+#include "NetClient.h"
+#include <cassert>
 
 using namespace std;
 using namespace std::chrono;
@@ -38,11 +40,11 @@ uint32_t get_id_mod(string query_id, uint32_t number_of_groups)
 
 void process_ids(uint32_t number_of_groups){
 
-    string id_file = "../query_data.csv";
+    string id_file = "query_data.csv";
     string one_line;
     //一个数组，记录每个文件的index
-    uint32_t * index = new uint32_t [90];
-    for (int i = 0; i < 90; ++i) {
+    uint32_t * index = new uint32_t [number_of_groups];
+    for (int i = 0; i < number_of_groups; ++i) {
         index[i]=0;
     }
 
@@ -51,13 +53,13 @@ void process_ids(uint32_t number_of_groups){
     ofstream write_map[number_of_groups];
     for (int i = 0; i < number_of_groups; ++i) {
         char path[40];
-        sprintf(path, "id_map_%d.data", i);
+        sprintf(path, "id_map/id_map_%d.data", i);
         write_map[i].open(path, ofstream::out|ofstream::app);
     }
 
 
     getline(query_data, one_line); //跳过首行‘id’
-   while (getline(query_data, one_line)) {
+    while (getline(query_data, one_line)) {
         string one_id = one_line.substr(0, 18);
         uint32_t one_id_mod = get_id_mod(one_id, number_of_groups);
         write_map[one_id_mod]<<one_id<<" "<<index[one_id_mod]<<endl;
@@ -68,18 +70,20 @@ void process_ids(uint32_t number_of_groups){
         write_map[i].close();
     }
     ofstream count_id;
-    count_data.open("count_id.data");
+    count_id.open("id_map/count_id.data");
     for (int i = 0; i < number_of_groups; ++i) {
-        count_data<< i << " " << index[i]<<endl;
+        count_id<< i << " " << index[i]<<endl;
     }
+    cout<<"Finishing process ids"<<endl;
 }
 
+//返回query_id在文件中的index，并给number_of_items赋值
 uint32_t find_index(string query_id, uint32_t number_of_groups, uint32_t &number_of_items){
     uint32_t id_mod = get_id_mod(query_id, number_of_groups);
     cout << "id_mod:" << id_mod << endl;
 
     char path1[40];
-    sprintf(path1, "id_map_%d.data", id_mod);
+    sprintf(path1, "id_map/id_map_%d.data", id_mod);
     ifstream read_map;
     read_map.open(path1, ifstream::in);
     string id, index1;
@@ -92,7 +96,10 @@ uint32_t find_index(string query_id, uint32_t number_of_groups, uint32_t &number
     }
     read_map.close();
 
-    read_count.open("count_id.data", ifstream::in);
+    ifstream read_count;
+    string mod, mod_count;
+    uint32_t count = -1;
+    read_count.open("id_map/count_id.data", ifstream::in);
     while(read_count>>mod>>mod_count){
         if(id_mod == atoi(mod.c_str())){
             count = atoi(mod_count.c_str());
@@ -106,17 +113,33 @@ uint32_t find_index(string query_id, uint32_t number_of_groups, uint32_t &number
     return index;
 }
 
-int main(){
+int main(int argc, char* argv[]){
+    /*
+    if (argc<3) {
+        cout<<"Error:needs assign server's ip and port"<<endl;
+        return 0;
+    */
 
-    uint64_t number_of_items = 0;  //百万不可区分度， 具体需要从服务器获取
+    //啟動net_client 如 ./client 127.0.0.1 10010
+    char *ip="127.0.0.1";
+    int port=11111;
+    if (argc > 2) {
+        ip = argv[1];
+        port = atoi(argv[2]);
+    }
+    NetClient net_client(ip, port);
+    net_client.init_client();
+
+    uint32_t number_of_items = 0;  //百万不可区分度， 具体需要从服务器获取
     uint64_t size_per_item = 23;       //每条记录需要的占用23字节
     uint32_t N = 4096;
-    uint32_t number_of_groups = 90;
+    uint32_t number_of_groups = 95;
+
 
     //pre-process ids
     bool process_id = false;
     if(process_id) {
-        process_ids();
+        process_ids(number_of_groups);
     }
 
 
@@ -127,13 +150,21 @@ int main(){
     cin>> query_id;
     cout<<"query id:"<<query_id<<endl;
 
-    //获取query_id的sha256后的mod_id
-    //存储SHA256取模相同的一组id
-
     // the query index to be queried, and assign value to number of items
     uint32_t ele_index = find_index(query_id, number_of_groups, number_of_items);
+    while(ele_index==-1) {
+        cout<<"query_id not found, enter again:"<<endl;
+        cin>>query_id;
+        ele_index = find_index(query_id, number_of_groups, number_of_items);
+    }
 
-    // Recommended values: (logt, d) = (12, 2) or (8, 1).
+    //获取query_id的sha256后的mod_id, 并发送给server
+
+    uint32_t mod_id = get_id_mod(query_id, number_of_groups);
+    cout<<"sending mod_id to server"<<endl;
+    net_client.one_time_send((char *)&mod_id, sizeof(mod_id));
+
+    // 初始化加密参数和PIR参数
     uint32_t logt = 60;
     PirParams pir_params;
     EncryptionParameters parms(scheme_type::BFV);
@@ -143,35 +174,40 @@ int main(){
 
     // Initialize PIR client....
     pir_client client(parms, pir_params);
+
+    //生成Galois keys，并传给Server
     GaloisKeys galois_keys = client.generate_galois_keys();
+    cout << "Client: Setting Galois keys...";
+    cout<<"sending galois_keys to server"<<endl;
+    net_client.one_time_send((char *)&galois_keys, sizeof(galois_keys));
 
-    //Galois keys需要传给Server，待补充
-
-    cout << "Main: Setting Galois keys...";
-
-
-
-    if(ele_index==-1){
-        cout<<"query_id not existed!"<<endl;
-        return 0;
-    }
 
 
     uint64_t index = client.get_fv_index(ele_index, size_per_item);   // index of FV plaintext
     uint64_t offset = client.get_fv_offset(ele_index, size_per_item);
 
-    cout << "Main: element index in the chosen Group = " << ele_index << " from [0, "
+    cout << "Client: element index in the chosen Group = " << ele_index << " from [0, "
     << number_of_items -1 << "]" << endl;
-    cout << "Main: FV index = " << index << ", FV offset = " << offset << endl;
+    cout << "Client: FV index = " << index << ", FV offset = " << offset << endl;
 
+    //生成sk的密文，并传给server
+    GSWCiphertext enc_sk=client.get_enc_sk();
+    cout<<"enc_sk size:"<<enc_sk.size()<<endl;
+    cout<<"sending enc_sk to server"<<endl;
+    for (int i = 0; i < enc_sk.size(); ++i) {
+        Ciphertext ct = enc_sk[i];
+        net_client.one_time_send((char *)&ct, sizeof(ct));
+    }
 
     auto time_query_s = high_resolution_clock::now();
     PirQuery query = client.generate_query_combined(index);
-
-    cout<<"Main: query size = "<< query.size()<< endl;
-
-    //传给server
-    GSWCiphertext enc_sk=client.get_enc_sk();
+    cout<<"Client: query size = "<< query.size()<< endl;
+    cout<<"sending pir query to server"<<endl;
+    //传query给server，传两个密文
+    for (int i = 0; i < 2; ++i) {
+        Ciphertext ct = query[i][0];
+        net_client.one_time_send((char *)&ct, sizeof(ct));
+    }
 
 
     auto time_query_e = high_resolution_clock::now();
@@ -181,19 +217,27 @@ int main(){
 
     //从server获取reply
     PirReply reply;
+    Ciphertext ct;
+    cout<<"Receiving pir reply from server"<<endl;
+    uint32_t recv_length = net_client.one_time_receive();
+    assert(recv_length == sizeof(ct));
+    memcpy(&ct, net_client.buffer, sizeof(ct));
+    reply.push_back(ct);
+
     Plaintext rep= client.decrypt_result(reply);
 
     // Convert from FV plaintext (polynomial) to database element at the client
     vector<uint8_t> elems(N * logt / 8);
     coeffs_to_bytes(logt, rep, elems.data(), (N * logt) / 8);
 
-    // Check that we retrieved the correct element
-    for (uint32_t i = 0; i < size_per_item; i++) {
-        cout<<"pir reply decrypted result:"<<endl;
-        cout<<elems[(offset * size_per_item) + i];
+    char pt[size_per_item];
+    for (uint32_t i = 0; i < size_per_item; ++i) {
+        pt[i] = elems[(offset * size_per_item) + i];
     }
+    cout<<"Retrived data:"<<pt<<endl;
 
     //cout << "Main: Reply num ciphertexts: " << reply.size() << endl;
 
     return 0;
 }
+
