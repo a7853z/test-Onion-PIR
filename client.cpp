4a23e900b60508d57c8984f9fcfa48fcd6887f49
a7853z <549613345@qq.com>
2022-09-07 19:33:20 +0800

    add some functions on the client side

--- client.cpp
+++ client.cpp
@@ -22,6 +22,7 @@
 #include <cassert>
 #include <sstream>
 #include <pthread.h>
+#include <mutex>
 
 using namespace std;
 using namespace std::chrono;
@@ -29,6 +30,10 @@ using namespace seal;
 using namespace seal::util;
 
 typedef vector<Ciphertext> GSWCiphertext;
+set<uint32_t> batch_query_index;
+set<uint64_t> batch_id_set;
+uint64_t batch_query_ids[10000];
+mutex batch_result_mutex;
 
 void process_ids(uint32_t number_of_groups){
 
@@ -198,6 +203,85 @@ void one_time_query(pir_client &client, NetClient &net_client, string query_id){
     //cout << "Main: Reply num ciphertexts: " << reply.size() << endl;
 }
 
+void handle_one_batch_query(pir_client & client, uint32_t query_index, uint32_t position) {
+
+    NetClient net_client(ip, port);
+    net_client.init_client();
+
+    uint64_t index = query_index;
+    cout << "Client:: Query index" << index << endl;
+
+    auto time_query_s = high_resolution_clock::now();
+    PirQuery query = client.generate_query_combined(index);
+    cout<<"Client:: query size = "<< query.size()<< endl;
+    for (int i = 0; i < query.size(); ++i) {
+        cout<<"query["<<i<<"] size:"<<query[i].size()<<endl;
+    }
+    cout<<"Client:: sending one batch pir query to server"<<endl;
+    //传query给server，传两个密文
+    int query_size=0;
+    for (int i = 0; i < 2; ++i) {
+        stringstream ct_stream;
+        Ciphertext ct = query[i][0];
+        uint32_t ct_size = ct.save(ct_stream);
+        query_size += ct_size;
+        string ct_string = ct_stream.str();
+        const char * ct_temp = ct_string.c_str();
+        net_client.one_time_send(ct_temp, ct_size);
+        //清空
+        ct_string.clear();
+        ct_stream.clear();
+        ct_stream.str("");
+    }
+    auto time_query_e = high_resolution_clock::now();
+    auto time_query_us = duration_cast<microseconds>(time_query_e - time_query_s).count();
+    cout<<fixed<<setprecision(3)<<"Client: query generated, total bytes:"
+        <<query_size/1024.0<<"KB"<<endl;
+    cout << "Client: PIRClient query generation time: " << time_query_us / 1000 << " ms" << endl;
+
+    //从server获取reply
+    auto time_reply_s = high_resolution_clock::now();
+    PirReply reply;
+    Ciphertext ct;
+    stringstream ct_stream;
+    string ct_string;
+    net_client.one_time_receive(ct_string);
+    ct_stream<<ct_string;
+    ct.load(client.newcontext_, ct_stream);
+    ct_stream.clear();
+    ct_stream.str("");
+    ct_string.clear();
+    reply.push_back(ct);
+    cout<<"Client:: Receiving pir reply from server"<<endl;
+    auto time_reply_e = high_resolution_clock::now();
+    auto time_reply_us = duration_cast<microseconds>(time_reply_e - time_reply_s).count();
+    cout << "Client: Server reply time: " << time_reply_us / 1000 << " ms" << endl;
+
+    Plaintext rep= client.decrypt_result(reply);
+
+    // Convert from FV plaintext (polynomial) to database element at the client
+    vector<uint8_t> elems(N * logt / 8);
+    coeffs_to_bytes(logt, rep, elems.data(), (N * logt) / 8);
+
+    uint32_t ele_per_ptxt = elements_per_ptxt(logt, N, size_per_item);
+    char pt[size_per_item];
+    uint64_t batch_id;
+
+    lock_guard<mutex> lock(batch_result_mutex);
+    char path[40];
+    path = "batch_query_result.data";
+    write_batch_result.open(path, ios::out|ios::app);
+    for (int i = 0; i < ele_per_ptxt; ++i) {
+        batch_id = batch_query_ids[position*ele_per_ptxt+i]
+        for (int j = 0; j < size_per_item; ++j) {
+            pt[i] = elems[(i * size_per_item) + j];
+        }
+        write_batch_query<<batch_id<<" "<<pt<< endl;
+    }
+    write_batch_result.close();
+}
+
+
 uint64_t * process_batch_ids(){
     string one_line;
     uint32_t batch_size =10000;
@@ -219,6 +303,7 @@ uint64_t * process_batch_ids(){
     }
     query_id.close();
     cout<<"client::read "<<batch_id_set.size()<< " ids"<<endl;
+
     return batch_id_array;
     cout<<"client::Finishing reading batch_ids:"<<count<<endl;
 }
@@ -233,7 +318,7 @@ uint64_t * random_pick() {
     group_ids = new uint64_t *[number_of_groups];
 
     uint32_t * id_count = new uint32_t [number_of_groups];  //存储id_count数据
-    cout<<"read id_count"<<endl;
+    cout<<"client::read all groups id_count"<<endl;
     ifstream read_count;
     read_count.open("id_map/count_data.data", ifstream::in);
     for (int i = 0; i < number_of_groups; ++i) {
@@ -245,7 +330,7 @@ uint64_t * random_pick() {
     }
     read_count.close();
 
-    cout<<"read all ids"<<endl;
+    cout<<"client::read all ids"<<endl;
     ifstream read_map[number_of_groups];
     stringstream id_stream;
     for (int i = 0; i < number_of_groups; ++i) {
@@ -291,45 +376,68 @@ uint64_t * random_pick() {
     return random_id_array;
 }
 
-int main(int argc, char* argv[]){
-    ConfigFile::set_path("client.conf");
-    ConfigFile config = ConfigFile::get_instance();
-    if(config.key_exist("N")) N = config.get_value_uint32("N");
-    if(config.key_exist("logt")) logt = config.get_value_uint32("logt");
-    if(config.key_exist("size_per_item")) size_per_item = config.get_value_uint64("size_per_item");
-    if(config.key_exist("number_of_groups")) number_of_groups = config.get_value_uint32("number_of_groups");
-    if(config.key_exist("id_file")) id_file = config.get_value("id_file");
-    if(config.key_exist("batch_id_file")) batch_id_file = config.get_value("batch_id_file");
+void batch_id_index_process() {
+    batch_query_ids = process_batch_ids();
+    uint64_t * random_pick_ids = random_pick();
+    auto ele_per_ptxt = elements_per_ptxt(logt, N, size_per_item);  //1024 = 4096/4
+    uint32_t query_number = ceil(10000/(double)ele_per_ptxt);
+    uint32_t ptxt_number = ceil(1000000/(double)ele_per_ptxt);
+    ofstream write_batch_query;
+    char path[40];
+    path = "batch_query_proprocess.data";
+    write_batch_query.open(path, ofstream::out);
 
-    if (argc>1 and argv[1]=="batch") {
-
+    //生成随机query_index
+    random_device rd;
+    for (int i = 0; i < query_number; ++i) {
+        uint32_t query_index = rd() % ptxt_number;
+        batch_query_index.insert(query_index);
+        write_batch_query<<query_index<< endl;
     }
 
-    //从conf文件获取 ip和port, 否则默认值127.0.0.1：11111
-    if(config.key_exist("ip")) ip = config.get_value("ip");
-    if(config.key_exist("port")) port = config.get_value_int("port");
-
-    NetClient net_client(ip, port);
-    net_client.init_client();
-
-    uint32_t number_of_items = 0;  //百万不可区分度， 具体需要从服务器获取
-
-    //pre-process ids
-    bool process_id = false;
-    if(process_id) {
-        process_ids(number_of_groups);
+    //写100w  id-index 到文件
+    uint32_t current_pt_index;
+    uint32_t batch_id_idx = 0;
+    uint32_t random_id_idx = 0;
+    for (int i = 0; i < 1000000; ++i) {
+        current_pt_index = i/ele_per_ptxt;
+        //取batch_id
+        if(batch_query_index.count(current_pt_index)!=0 and batch_id_idx<10000) {
+            write_batch_query<<batch_query_ids[batch_id_idx]<<" "<<i<< endl;
+            ++batch_id_idx;
+        } else {
+            write_batch_query<<random_pick_ids[random_id_idx]<<" "<<i<< endl;
+            ++random_id_idx;
+        }
     }
 
-    // 初始化加密参数和PIR参数
-    PirParams pir_params;
-    EncryptionParameters parms(scheme_type::BFV);
-    set_bfv_parms(parms);   //N和logt在这里设置
-    gen_params(number_of_items,  size_per_item, N, logt,
-               pir_params);
+    delete [] random_pick_ids;
+}
 
-    // Initialize PIR client....
-    pir_client client(parms, pir_params);
+//
+char * get_batch_query_buffer() {
+    auto ele_per_ptxt = elements_per_ptxt(logt, N, size_per_item);  //1024 = 4096/4
+    uint32_t query_number = ceil(10000/(double)ele_per_ptxt);
+    uint32_t ptxt_number = ceil(1000000/(double)ele_per_ptxt);
+    set<uint32_t> query_index;
+    ifstream query_data("batch_query_proprocess.data", ifstream::in);
+    uint32_t index;
+    for (int i = 0; i < query_number; ++i) {
+        query_data>>index;
+    }
 
+    char buffer[1000000*8];  //send 1million 8bytes-id to server in order.
+    uint64_t id;
+    uint32_t offset=0;
+    for (int i = 0; i < 1000000; ++i) {
+        query_data >> id >> index;
+        memcpy(buffer+offset, (char*)&id ,sizeof(id));
+    }
+    query_data.close();
+    return buffer;
+}
+
+void send_public_params (pir_client &client, NetClient &net_client){
     //生成Galois keys，并传给Server
     GaloisKeys galois_keys = client.generate_galois_keys();
     cout << "Client: Setting Galois keys...";
@@ -338,8 +446,8 @@ int main(int argc, char* argv[]){
     string g_string = galois_stream.str();
     const char * galois_temp = g_string.c_str();
 
-    cout<<fixed<<setprecision(3)<<"Client: Sending galois_keys to server, key size:"
-    <<galois_size/1024.0<<"KB"<<endl;
+    cout<<fixed<<setprecision(3)<<"Client:: Sending galois_keys to server, key size:"
+        <<galois_size/1024.0<<"KB"<<endl;
 
     net_client.one_time_send(galois_temp, galois_size);
     //清空stream和string
@@ -366,7 +474,86 @@ int main(int argc, char* argv[]){
         ct_stream.clear();
         ct_stream.str("");
     }
-    cout<<fixed<<setprecision(3)<<"Client: enc_sk size:"<<enc_sk_size/1024.0<<"KB"<<endl;
+    cout<<fixed<<setprecision(3)<<"Client:: enc_sk size:"<<enc_sk_size/1024.0<<"KB"<<endl;
+}
+
+int handle_batch_query() {
+    //send public param first
+    NetClient net_client(ip, port);
+    net_client.init_client();
+    // 初始化加密参数和PIR参数
+    PirParams pir_params;
+    EncryptionParameters parms(scheme_type::BFV);
+    set_bfv_parms(parms);   //N和logt在这里设置
+    gen_params(1000000,  size_per_item, N, logt,
+               pir_params);
+
+    // Initialize PIR client....
+    pir_client client(parms, pir_params);
+    send_public_params(client, net_client);
+
+    batch_id_index_process();
+    char * buffer = get_batch_query_buffer();
+    uint32_t buffer_size = 1000000*8; //1million uint64_t
+    net_client.one_time_send(buffer, buffer_size);
+    close(net_client.connect_fd);
+    delete buffer;
+    buffer = Null;
+    set<uint32_t>::iterator iter;
+    uint32_t query_index;
+    uint32_t position = 0;
+    for (iter = batch_query_index.begin(); iter != batch_query_index.end(); iter++) {
+        query_index = *iter;
+        thread t(handle_one_batch_query, client, query_index, position);
+        position+=1;
+        t.join();
+    }
+    //write result to file
+
+    return 1;
+}
+
+
+int main(int argc, char* argv[]){
+    ConfigFile::set_path("client.conf");
+    ConfigFile config = ConfigFile::get_instance();
+    if(config.key_exist("N")) N = config.get_value_uint32("N");
+    if(config.key_exist("logt")) logt = config.get_value_uint32("logt");
+    if(config.key_exist("size_per_item")) size_per_item = config.get_value_uint64("size_per_item");
+    if(config.key_exist("number_of_groups")) number_of_groups = config.get_value_uint32("number_of_groups");
+    if(config.key_exist("id_file")) id_file = config.get_value("id_file");
+    if(config.key_exist("batch_id_file")) batch_id_file = config.get_value("batch_id_file");
+
+    //从conf文件获取 ip和port, 否则默认值127.0.0.1：11111
+    if(config.key_exist("ip")) ip = config.get_value("ip");
+    if(config.key_exist("port")) port = config.get_value_int("port");
+
+    if (argc>1 and argv[1]=="batch") {
+        return handle_batch_query();
+    }
+
+    NetClient net_client(ip, port);
+    net_client.init_client();
+
+    uint32_t number_of_items = 0;  //百万不可区分度， 具体需要从服务器获取
+
+    //pre-process ids
+    bool process_id = false;
+    if(process_id) {
+        process_ids(number_of_groups);
+    }
+
+    // 初始化加密参数和PIR参数
+    PirParams pir_params;
+    EncryptionParameters parms(scheme_type::BFV);
+    set_bfv_parms(parms);   //N和logt在这里设置
+    gen_params(number_of_items,  size_per_item, N, logt,
+               pir_params);
+
+    // Initialize PIR client....
+    pir_client client(parms, pir_params);
+
+    send_public_params(client, net_client);
 
     while(true){
         string query_id;
